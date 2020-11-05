a0de9d2ace0a1012f9fa296fe18f78f825607768
Dalton Morrel <daltonmorrel7&@gmail.com>
2020-11-05 11:39:39 -0500

    added highlight feature when drag/drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,6 +212,7 @@ var dropZoneDragHandler = function(event)
     if (taskListEl)
     {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -236,8 +237,19 @@ var dropTaskHandler = function(event)
     {
         statusSelectEl.selectedIndex = 2;
     }
+
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 };
+
+var dragLeaveHandler = function(event)
+{
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl)
+    {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -245,3 +257,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler)
